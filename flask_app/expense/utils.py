81209614ce99d4b50b2111b4c745598706ed75e7
Mikhail Salikov <misha.salikov@yandex.ru>
2020-05-14 12:38:52 +0300

    Добавлен файл utils.py, в него перемещен запрос  к БД

---
+++ flask_app/expense/utils.py
@@ -0,0 +1,9 @@
+from models import Expense
+from app import db
+from sqlalchemy import extract
+from datetime import datetime
+
+
+def get_list_expense_users(id_user):
+    expenses = db.session.query(Expense).filter(Expense.id_user == id_user, extract('month',Expense.created_date) == datetime.today().month, extract('year', Expense.created_date) == datetime.today().year).order_by(Expense.created_date.desc()).all()
+    return expenses
